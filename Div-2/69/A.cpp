1b8a446145c3e8d171351ad0d8188339393ff1b4
Faheel Ahmad <faheel@live.in>
2017-03-14 00:32:07 +0530

    Add solution for 69A

---
+++ Div-2/69/A.cpp
@@ -0,0 +1,16 @@
+/*
+    Problem link:
+    http://codeforces.com/problemset/problem/80/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int complete = 0;
+
+    return 0;
+}
