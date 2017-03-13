70afe5a691f7721a186e416d291557b7c7e12c70
Faheel Ahmad <faheel@live.in>
2017-03-14 00:31:16 +0530

    Add solution for 172A

---
+++ Div-2/172/A.cpp
@@ -0,0 +1,21 @@
+/*
+    Problem link:
+    http://codeforces.com/problemset/problem/281/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    string word;
+    cin >> word;
+
+    word[0] = toupper(word[0]);
+
+    cout << word << "\n";
+
+    return 0;
+}
