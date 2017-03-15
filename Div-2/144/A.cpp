61591ffb2652a4a913538ff3c9203fe1c2f25ea9
Faheel Ahmad <faheel@live.in>
2017-03-15 17:41:00 +0530

    Add solution for 144A

---
+++ Div-2/144/A.cpp
@@ -0,0 +1,24 @@
+/*	
+	Problem link:
+	http://codeforces.com/problemset/problem/233/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	
+	int perm_size;
+	cin >> perm_size;
+
+	if (perm_size % 2 == 0)
+		for (int num = perm_size; num > 0; num--)
+			cout << num << " ";
+	else
+		cout << -1;
+	cout << "\n";
+	
+	return 0;
+}
